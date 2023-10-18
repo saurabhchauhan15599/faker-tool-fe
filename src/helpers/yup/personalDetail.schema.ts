2b474316470f4abc7bdcd417aec0a8da9bebba4f
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2023-10-18 16:57:13 +0530

    build added

--- src/helpers/yup/personalDetail.schema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const PersonalDetailsSchema = yup.object().shape({
-  name: yup.string().trim().required('Name is required').matches(/^[a-z A-Z]+$/, 'Not a valid name'),
-  email: yup.string().email().trim().required('Email id is required'),
-});
