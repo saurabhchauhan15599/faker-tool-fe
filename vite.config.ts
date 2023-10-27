8bea767941f44d24c632d9e887ab645ee82d99d2
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2023-10-27 20:41:27 +0530

    updated config and package

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: 'REACT_APP_',
+  envPrefix: "REACT_APP_",
   plugins: [react()],
   build: {
-    outDir: 'build',
+    outDir: "../dist",
   },
   server: {
     port: 3000,
     open: true,
     headers: {
-      'X-Frame-Options': 'none',
-      'X-Content-Type-Options': 'nosniff',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'unsafe-none',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': '*',
-      'Access-Control-Allow-Headers': '*',
+      "X-Frame-Options": "none",
+      "X-Content-Type-Options": "nosniff",
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
     },
   },
-})
+});
