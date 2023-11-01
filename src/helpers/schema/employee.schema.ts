f1c7d75b1f0a7ba9b0997a130769b351abdabf04
Saurabh Chauhan <saurabh.chauhan@coditas.com>
2023-11-01 16:13:54 +0530

    minor fixes added

--- src/helpers/schema/employee.schema.ts
+++ src/helpers/schema/employee.schema.ts
@@ -3,17 +3,22 @@ import * as yup from "yup";
 export const employeeFormSchema = yup.object().shape({
   limit: yup
     .string()
-    .trim()
-    .typeError("Limit should be number")
+    .matches(/^\d+$/, "Limit should only contain numbers")
     .required("Field is required!"),
   min: yup
     .string()
-    .trim()
     .required("Field is required!")
+    .matches(/^\d+$/, "Limit should only contain numbers")
+    .typeError("Limit should be number")
     .min(0, "Can't be less than zero!"),
   max: yup
     .string()
-    .trim()
     .required("Field is required!")
+    .matches(/^\d+$/, "Limit should only contain numbers")
     .max(99999999999999, "Can't be more than 14 digits"),
+  designation: yup.string().required("Field is required!"),
+});
+
+export const employeeSchema = yup.object().shape({
+  employees: yup.array().of(employeeFormSchema),
 });
